f7bcd94337058351d5796ce89c449608ee387a87
Zaeema Anwar <thezaeemaanwar@gmail.com>
2022-02-14 00:00:15 +0500

    🐛 FIX: Toolbar position

--- src/Components/Toolbar/ToolBar.js
+++ src/Components/Toolbar/ToolBar.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 const ToolBar = ({ addQuestion }) => {
   return (
     <div
-      className={`h-12 w-11/12 px-3 sm:w-12 sm:h-60 sm:py-3 bg-white rounded m-3 flex flex-row justify-between sm:flex-col items-center text-fontGrey border border-hoverGrey shadow-md`}
+      className={`h-12 w-11/12 px-3 sm:w-12 sm:h-60 sm:py-3 sm:sticky sm:top-32 bg-white rounded m-3 flex flex-row justify-between sm:flex-col items-center text-fontGrey border border-hoverGrey shadow-md`}
     >
       {toolBarActions.map((action) => (
         <div key={action.id} title={action.label}>
